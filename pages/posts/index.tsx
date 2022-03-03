d65d7f26c860666f899db791040ea83dec46b625
Cong quy <congquy2509@gmail.com>
2022-03-03 17:37:50 +0700

    updateCode

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { GetStaticPropsContext, GetStaticProps } from "next";
 
-function PostListPage() {
+export interface PostListPageProps {}
+
+export default function PostListPage(props: PostListPageProps) {
   return <div>Post List Page</div>;
 }
 
-export default PostListPage;
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // Get theserver-side
+  // build-time
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1"
+  );
+  const data = await response.json();
+  console.log("🚀 ~ data", data);
+  return {
+    props: {
+      posts: [],
+    }, // will be passed to the page component as props
+  };
+};
